4996dcc6d043609ea5347eff167fd4d0da0f8c91
Krister Kari <krister.kari@rovio.com>
2015-02-25 23:31:54 +0200

    Add test task

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,7 @@ var gulp = require('gulp');
 gulp.task('default', function() {
     console.log("running default task");
 });
+
+gulp.task('test', function() {
+    console.log("running test task");
+});
