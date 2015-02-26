858a7d15caf1b69ecb5ff00d593373412ea37ffa
Krister Kari <krister.kari@rovio.com>
2015-02-27 00:39:09 +0200

    Add SauceLabs integration

---
+++ config/karma.sauce.conf.js
@@ -0,0 +1,43 @@
+"use strict";
+
+module.exports = function(config) {
+
+    var customLaunchers = {
+        iOSSafari: {
+            base: "SauceLabs",
+            browserName: "iphone",
+            version: "5.1"
+        },
+        ie7: {
+            base: "SauceLabs",
+            browserName: "internet explorer",
+            version: "7.0"
+        },
+        ie8: {
+            base: "SauceLabs",
+            browserName: "internet explorer",
+            version: "8.0"
+        },
+        ie9: {
+            base: "SauceLabs",
+            browserName: "internet explorer",
+            version: "9.0"
+        }
+    };
+
+    config.set({
+        basePath: "",
+        frameworks: ["jasmine"],
+        captureTimeout: 120000,
+        files: [
+            "../test/**/*_spec.js"
+        ],
+        sauceLabs: {
+            testName: "HTML5 placeholder polyfill"
+        },
+        customLaunchers: customLaunchers,
+        browsers: Object.keys(customLaunchers),
+        reporters: ["dots", "saucelabs"],
+        singleRun: true
+    });
+};
