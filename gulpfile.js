58df20511f80ac3c05c7e472c8e774b1fd5d215a
Krister Kari <krister.kari@rovio.com>
2015-02-28 23:52:07 +0200

    Change to use "use strict" in function form

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,35 @@
-"use strict";
+(function() {
+    "use strict";
 
-var path = require("path");
-var gulp = require("gulp");
-var jscs = require("gulp-jscs");
-var eslint = require("gulp-eslint");
-var karma = require("karma").server;
+    var path = require("path");
+    var gulp = require("gulp");
+    var jscs = require("gulp-jscs");
+    var eslint = require("gulp-eslint");
+    var karma = require("karma").server;
 
-gulp.task("default", function() {
-    return gulp.src("*.js")
-               .pipe(eslint({
-                   useEslintrc: true
-               }))
-               .pipe(eslint.format())
-               .pipe(eslint.failAfterError())
-               .pipe(jscs());
-});
+    gulp.task("default", function() {
+        return gulp.src("*.js")
+                   .pipe(eslint({
+                       useEslintrc: true
+                   }))
+                   .pipe(eslint.format())
+                   .pipe(eslint.failAfterError())
+                   .pipe(jscs());
+    });
 
-gulp.task("test", function(done) {
-    karma.start({
-        configFile: path.join(__dirname, "/config/karma.conf.js"),
-        singleRun: true
-    }, done);
-});
+    gulp.task("test", function(done) {
+        karma.start({
+            configFile: path.join(__dirname, "/config/karma.conf.js"),
+            singleRun: true
+        }, done);
+    });
 
-gulp.task("tdd", function(done) {
-    karma.start({
-        configFile: path.join(__dirname, "/config/karma.conf.js"),
-        singleRun: false,
-        autoWatch: true
-    }, done);
-});
+    gulp.task("tdd", function(done) {
+        karma.start({
+            configFile: path.join(__dirname, "/config/karma.conf.js"),
+            singleRun: false,
+            autoWatch: true
+        }, done);
+    });
+
+}());
