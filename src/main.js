23b68a94cf86ff2a81d7473307488150115b00d3
Krister Kari <krister.kari@rovio.com>
2015-03-09 21:16:25 +0200

    refactor/add function to get placeholder value

--- src/main.js
+++ src/main.js
@@ -30,11 +30,17 @@
         return isFocusEnabled;
     }
 
-    function hasPlaceholderAttrSet(element) {
-        return Boolean("placeholder" in element &&
-               element.placeholder !== "" ||
+    function getPlaceholderValue(element) {
+        return "placeholder" in element &&
+               element.placeholder ||
+               // IE10 emulating IE7 fails with getAttribute, hence the use of the attributes node
+               // IE returns an empty object instead of undefined if the attribute is not present
                element.attributes.placeholder &&
-               element.attributes.placeholder.nodeValue !== "");
+               element.attributes.placeholder.nodeValue;
+    }
+
+    function hasPlaceholderAttrSet(element) {
+        return Boolean(getPlaceholderValue(element));
     }
 
     function getElementType(element) {
