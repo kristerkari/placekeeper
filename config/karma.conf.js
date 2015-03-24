9cf331b7566392fe04070d84bb578a37eb57b7c1
Krister Kari <krister.kari@rovio.com>
2015-03-24 09:28:38 +0200

    Fix styling for Karma config

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -2,80 +2,72 @@
 // Generated on Thu Feb 26 2015 23:29:38 GMT+0200 (EET)
 
 module.exports = function(config) {
-  config.set({
+    "use strict";
+    config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: "",
 
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ["jasmine"],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      '../src/support.js',
-      '../src/utils.js',
-      '../src/polyfill.js',
-      '../src/main.js',
-      '../test/**/*_spec.js'
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            "../src/support.js",
+            "../src/utils.js",
+            "../src/polyfill.js",
+            "../src/main.js",
+            "../test/**/*_spec.js"
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      // source files, that you wanna generate coverage for
-      // do not include tests or libraries
-      // (these files will be instrumented by Istanbul)
-      '../src/**/!(support).js': ['coverage']
-    },
+        // list of files to exclude
+        exclude: [
+        ],
 
-    // optionally, configure the reporter
-    coverageReporter: {
-      dir : '../coverage/',
-      reporters: [
-          { type: 'lcov' },
-          { type: 'text-summary' }
-      ]
-    },
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            "../src/**/!(support).js": ["coverage"]
+        },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+        // optionally, configure the reporter
+        coverageReporter: {
+            dir: "../coverage/",
+            reporters: [
+                { type: "lcov" },
+                { type: "text-summary" }
+            ]
+        },
 
+        // test results reporter to use
+        // possible values: "dots", "progress"
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ["progress", "coverage"],
 
-    // web server port
-    port: 9876,
+        // web server port
+        port: 9876,
 
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ["PhantomJS"],
 
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
+    });
 };
