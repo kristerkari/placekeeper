b2631f8981196975c276520064dbdd34418ca070
Krister Kari <krister.kari@webtehdas.fi>
2015-06-14 21:33:43 +0300

    Rebuild jQuery adapter

--- dist/placekeeper.jquery.js
+++ dist/placekeeper.jquery.js
@@ -1076,30 +1076,40 @@
   var originalValFn = $.fn.val;
   var originalPropFn = $.fn.prop;
 
-  if (!global.placekeeper.support.hasNativePlaceholderSupport()) {
+  function setup() {
+    if (!global.placekeeper.support.hasNativePlaceholderSupport()) {
 
-    $.fn.val = function(val) {
-      var originalValue = originalValFn.apply(this, arguments);
-      var placeholder = this.eq(0).data("placeholder-value");
-      if (
-        val === undefined &&
-        this.eq(0).data("placeholder-active") &&
-        originalValue === placeholder
-      ) {
-        return "";
-      }
-      return originalValue;
-    };
+      $.fn.val = function(val) {
+        var originalValue = originalValFn.apply(this, arguments);
+        var placeholder = this.eq(0).data("placeholder-value");
+        if (
+          val === undefined &&
+          this.eq(0).data("placeholder-active") &&
+          originalValue === String(placeholder)
+        ) {
+          return "";
+        }
+        return originalValue;
+      };
 
-    $.fn.prop = function(name, val) {
-      if (
-        val === undefined &&
-        this.eq(0).data("placeholder-active") &&
-        name === "value"
-      ) {
-        return "";
-      }
-      return originalPropFn.apply(this, arguments);
-    };
+      $.fn.prop = function(name, val) {
+        if (
+          val === undefined &&
+          this.eq(0).data("placeholder-active") &&
+          name === "value"
+        ) {
+          return "";
+        }
+        return originalPropFn.apply(this, arguments);
+      };
+
+    }
   }
+
+  setup();
+
+  global.placekeeper.jQuery = {
+    setup: setup
+  };
+
 }(this.jQuery, this));
